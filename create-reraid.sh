ab600730bfa89b8861bc36bb94a4c6ae97aca776
Airamek <vegkeppnemairamek@gmail.com>
2021-05-21 15:22:59 +0200

    A wrapper for the scripts in setupscripts/

---
+++ create-reraid.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+while [ ! -z $1 ]; do
+    case $1 in 
+        -d | delete)
+        # Delete all work files
+            case $2 in
+                1)
+                    sudo rm -rf reraid/
+                    exit 0
+                ;;
+                2)
+                    sudo rm -rf reraid/
+                    rm -rf reraid-test.cpio.gz
+                    exit 0
+                ;;
+                3 | *)
+                    sudo rm -rf reraid/
+                    sudo rm -rf reraid-test.cpio.gz
+                    sudo rm -rf tmp
+                    exit 0
+                ;;
+            esac
+        ;;
+    esac
+done
+
+./setupscripts/reraid-stage1.sh
+./setupscripts/reraid-stage2.sh
+./setupscripts/reraid-stage3.sh
+./setupscripts/createmedia.sh -t optical
